2f1961469293648850db3b191165938682f54c15
zxinyi404-maker <zxinyi404@gmail.com>
2026-07-29 22:40:52 +0800

    chore: 升级到 v2.1.0 - 完整交互功能版本
    🎉 重大更新
    - 所有核心功能已完整实现
    - 评论、转发、点赞、书签、分享全部可用
    - 推文详情页完全符合 Twitter 体验
    
    📦 版本说明
    v2.1.0 是第一个功能完整的正式版本
    所有主要交互功能已经可以正常使用

--- plugin.js
+++ plugin.js
@@ -67,7 +67,7 @@ function showToast(message, type = 'success') {
   window.RochePlugin.register({
     id: PLUGIN_ID,
     name: 'Twitter',
-    version: '2.0.3',
+    version: '2.1.0',
     icon: '𝕏',
     apps: [{
       id: 'twitter-home',
