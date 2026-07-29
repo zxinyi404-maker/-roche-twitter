4e30095b2aaf96a45f96ad32c1b50a9c080915bd
zxinyi404-maker <zxinyi404@gmail.com>
2026-07-29 23:31:39 +0800

    chore: 升级到 v2.4.0 - 侧边栏菜单系统
    🎉 重大更新
    - 完整的侧边栏菜单系统
    - 点击头像打开侧边栏（不跳转页面）
    - 多账号快速切换功能
    
    📦 版本说明
    v2.4.0 实现了 Twitter 的侧边栏菜单
    完美的用户体验 + 账号切换功能

--- plugin.js
+++ plugin.js
@@ -67,7 +67,7 @@ function showToast(message, type = 'success') {
   window.RochePlugin.register({
     id: PLUGIN_ID,
     name: 'Twitter',
-    version: '2.3.1',
+    version: '2.4.0',
     icon: '𝕏',
     apps: [{
       id: 'twitter-home',
